6d8e95c7c02c7b8698bc42848b500f54cb8cf6f7
Daniel Martinez Maimone <96289800+ZapatoProgramming@users.noreply.github.com>
2024-04-29 23:47:18 -0600

    Add files via upload

---
+++ AztecasApp/app/src/main/java/com/example/ejemplonavigation/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.ejemplonavigation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.ejemplonavigation.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Encuentra el botón por su ID y configura un OnClickListener
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AfterLogin.class);
+                //intent.putExtra("clave", "valor");
+                startActivity(intent);
+                // Muestra un Toast cuando el botón es presionado
+                Toast.makeText(MainActivity.this, "Sesión iniciada", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
